5078e407eb31712cd7fe2d49edd776d9c8002071
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-23 21:52:08 -0300

    Cebolinha em açao

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cebolinha_AT_Etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string NomeDoUsuario; 
+
+        string SobreNomeDoUsuario;
+
+        string NomeCompleto;
+
+        string FrasesCebolinha;
+
+
+            Console.Write("Por Favor, digite seu nome: ");
+            NomeDoUsuario = Console.ReadLine();
+
+            Console.Write("Agora digite seu SobreNome: ");
+            SobreNomeDoUsuario = Console.ReadLine();
+
+            NomeCompleto = $"{NomeDoUsuario} {SobreNomeDoUsuario}";
+
+            Console.Write($"Ola {NomeCompleto},  Digite para mim alguma frase: ");
+            FrasesCebolinha = Console.ReadLine();
+
+            
+            Console.WriteLine(FrasesCebolinha.Replace("L" , "R"));
+        }
+    }
+}
